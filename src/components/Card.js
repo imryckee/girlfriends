00869fa085cf37e8e9398d623cb294b519fe3600
Ruiqi Xing <eva.rq.xing@gmail.com>
2020-02-26 20:31:22 +0800

    modify size

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Card = ({id,name,description,photo,rank}) => {
     return (
-        <div className='bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5' style={{width:'400px'}}>
-            <img alt={name} src={photo} height="200px"/>
+        <div className='bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5' style={{width:'280px'}}>
+            <img alt={name} src={photo} height="150px"/>
             <div>
-                <h2>{rank} : {name}({id})</h2>
+                <h3>{rank} : {name}({id})</h3>
                 <p>{description}</p>
             </div>
         </div>
