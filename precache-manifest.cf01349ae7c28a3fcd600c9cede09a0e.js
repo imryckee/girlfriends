795d5b32ec0b75b39a761f2d1d7c49fd8bb661ba
Ruiqi Xing <eva.rq.xing@gmail.com>
2020-02-26 15:57:31 +0800

    Updates

---
+++ precache-manifest.cf01349ae7c28a3fcd600c9cede09a0e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d9863fcd25f30df2f1dd0a26a7665a1",
+    "url": "/girlfriends/index.html"
+  },
+  {
+    "revision": "f219d251294c1d57093a",
+    "url": "/girlfriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "217bef1bce06ac80828f",
+    "url": "/girlfriends/static/css/main.8a81980f.chunk.css"
+  },
+  {
+    "revision": "f219d251294c1d57093a",
+    "url": "/girlfriends/static/js/2.feb90a1b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/girlfriends/static/js/2.feb90a1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "217bef1bce06ac80828f",
+    "url": "/girlfriends/static/js/main.fc5f2724.chunk.js"
+  },
+  {
+    "revision": "71fc6980352460b6f1fa",
+    "url": "/girlfriends/static/js/runtime-main.9e1e80f9.js"
+  },
+  {
+    "revision": "09f728822adc606ba3e06a3c5c0a3957",
+    "url": "/girlfriends/static/media/SamsungSans-Regular.09f72882.woff"
+  }
+]);
